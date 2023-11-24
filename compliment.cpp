20f42cc6d2d99643b80905f66814789867e35de2
Owais Ahmad <owaisahmad014@gmail.com>
2023-11-24 11:41:58 +0500

    finding compliment in C++

---
+++ compliment.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+	float pA, compliment_A;
+	
+		cout<<"ENTER PROBABILITY OF EVENT A= ";
+			cin>>pA;
+				compliment_A = 1-pA;
+					cout<<"THE COMPLIEMENT OF EVENT A IS = "<<compliment_A;
+	return 0;
+}
